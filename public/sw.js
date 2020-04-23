3340a5945a6211b88e01ad6ec23a790698289a34
husainasad <shusain_be16@thapar.edu>
2020-04-23 23:07:05 +0530

    Geolocation feature added

--- public/sw.js
+++ public/sw.js
@@ -194,6 +194,8 @@ self.addEventListener('sync', function(event) {
             postData.append('id', dt.id);
             postData.append('title', dt.title);
             postData.append('location', dt.location);
+            postData.append('rawLocationLat', dt.rawLocation.lat);
+            postData.append('rawLocationLng', dt.rawLocation.lng);
             postData.append('file', dt.picture, dt.id + '.png');
 
             fetch('https://us-central1-pwagram-18c54.cloudfunctions.net/storePostData', {
